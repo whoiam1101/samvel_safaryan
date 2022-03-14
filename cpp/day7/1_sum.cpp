a24881838daa6aa92548f8f34668b347540834aa
Samvel Safaryan <whoiam1101@outlook.com>
2022-03-14 19:25:38 +0400

    Implementation of sum functions using pointers and addresses

---
+++ cpp/day7/1_sum.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+void sum1(int *a, int *b, int *sum);
+void sum2(int &a, int &b, int &sum);
+void sum3(int  a, int  b, int &sum);
+void sum4(int  a, int *b, int &sum);
+void sum5(int *a, int *b, int &sum);
+
+void _beautifullPrint(string, int, int, int);
+
+int main() {
+    // For example
+    int a = 13;
+    int b = 44;
+    int sum;
+
+    sum1(&a, &b, &sum);
+    _beautifullPrint("sum1", a, b, sum);
+
+    sum2(a, b, sum);
+    _beautifullPrint("sum2", a, b, sum);
+
+    sum3(a, b, sum);
+    _beautifullPrint("sum3", a, b, sum);
+
+    sum4(a, &b, sum);
+    _beautifullPrint("sum4", a, b, sum);
+
+    sum5(&a, &b, sum);
+    _beautifullPrint("sum5", a, b, sum);
+
+    return 0;
+}
+
+void sum1(int *a, int *b, int *sum) {
+    *sum = *a + *b;
+}
+
+void sum2(int &a, int &b, int &sum) {
+    sum = a + b;
+}
+
+void sum3(int a, int b, int &sum) {
+    sum = a + b;
+}
+
+void sum4(int a, int *b, int &sum) {
+    sum = a + *b;
+}
+
+void sum5(int *a, int *b, int &sum) {
+    sum = *a + *b;
+}
+
+void _beautifullPrint(string s, int a, int b, int sum) {
+    cout << s << "(" << a << ", " << b << ") = " << sum << "\n";
+}
