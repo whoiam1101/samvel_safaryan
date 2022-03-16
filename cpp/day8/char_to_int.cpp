20b1048abd8122298447c5ab8400e836d6df7b3c
Samvel Safaryan <whoiam1101@outlook.com>
2022-03-16 21:45:25 +0400

    Change chat_to_int.cpp, for print sum all arguments

--- cpp/day8/char_to_int.cpp
+++ cpp/day8/char_to_int.cpp
@@ -8,10 +8,19 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     // If this expression evaluates to 0, this causes an assertion failure that terminates the program
-    assert(("Please, use one argument", argc == 2));
+    assert(("Please, use one argument", argc > 1));
 
-    // char[] -> int
-    cout << my_atoi(argv[1]) << endl;
+    int sum = 0;
+
+    for (int i = 1; i < argc - 1; i++) {
+        cout << my_atoi(argv[i]) << " + ";
+
+        sum += my_atoi(argv[i]);
+    }
+
+    sum += my_atoi(argv[argc - 1]);
+
+    cout << my_atoi(argv[argc - 1]) << " = " << sum << endl;
 
     return 0;
 }
