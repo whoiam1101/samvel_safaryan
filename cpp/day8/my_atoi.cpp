a31627fa6a0686842e29d5fbf7bc334e66ce9616
Samvel Safaryan <whoiam1101@outlook.com>
2022-03-16 21:44:56 +0400

    Changed my_atoi function

--- cpp/day8/my_atoi.cpp
+++ cpp/day8/my_atoi.cpp
@@ -1,25 +1,28 @@
-#include <cassert>
-
 #include "my_atoi.h"
 
 // Define my_atoi function
 int my_atoi(char* arr) {
-    // start pos = 1 if number is negative
-    // start pos = 0 if number is positive
-    int pos = (arr[0] == '-') ? 1 : 0;
+    int pos = 0;
+    bool is_negative = false;
+
+    while (arr[pos] != '\0' && !('0' <= arr[pos] && arr[pos] <= '9')) {
+        pos++;
+    }
+
     int answer = 0;
+
+    if (pos > 0 && arr[pos - 1] == '-') {
+        is_negative = true;
+    }
 
     // n = a_1 a_2 a_3 ... a_m
     // n = 10 * a_1 a_2 a_3 ... a_{n - 1} + a_n
-    while (arr[pos] != '\0') {
-        // If arr[pos] isn't a digit then exit program
-        assert(("Argument isn't a number", '0' <= arr[pos] && arr[pos] <= '9'));
-
+    while (arr[pos] != '\0' && '0' <= arr[pos] && arr[pos] <= '9') {
         answer = 10 * answer + (int) (arr[pos] - '0');
         pos++;
     }
 
-    if (arr[0] == '-') {
+    if (is_negative) {
         return -answer;
     }
 
