ddfb54d4411bdc2c44daff936f6a78ca892c7dc3
whoiam <whoiam1101@outlook.com>
2022-04-24 22:47:20 +0400

    Add all methods, constructors and destructor implementation in same header file

--- data structure/stack/stack.hpp
+++ data structure/stack/stack.hpp
@@ -38,7 +38,7 @@ public:
     void push(T);
 
     // pop back last element
-    void pop();
+    T pop();
 
     // peek top element
     T peek() const;
@@ -47,5 +47,34 @@ public:
     bool isEmpty() const;
 };
 
+// push back new element
+template<class T>
+void Stack<T>::push(T value) {
+    Node* top = new Node(value);
+    top->_next = _top;
+    _top = top;
+}
+
+// pop back last element
+template<class T>
+T Stack<T>::pop() {
+    T return_value = _top->_value;
+    Node* temporary_node = _top;
+    _top = _top->_next;
+    delete temporary_node;
+    return return_value;
+}
+
+// peek _top element
+template<class T>
+T Stack<T>::peek() const {
+    return _top->_value;
+}
+
+// is empty
+template<class T>
+bool Stack<T>::isEmpty() const {
+    return _top == nullptr;
+}
 
 #endif
