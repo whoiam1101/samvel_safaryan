36d9c8194305687089b2d3faaca772cd890dbc8f
whoiam <whoiam1101@outlook.com>
2022-04-28 22:35:20 +0400

    Implement bubble sort for int

---
+++ data structure/sort/selection_sort.hpp
@@ -0,0 +1,18 @@
+#ifndef __SELECTION_SORT_H__
+#define __SELECTION_SORT_H__
+
+void _selectionSort(int* array, int left, int right) {
+    for (int i = left; i < right; i++) {
+        for (int j = i + 1; j <= right; j++) {
+            if (array[i] > array[j]) {
+                std::swap(array[i], array[j]);
+            }
+        }
+    }
+}
+
+void _selectionSort(int* array, int size) {
+    _selectionSort(array, 0, size - 1);
+}
+
+#endif // __SELECTION_SORT_H__
