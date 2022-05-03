57956be72a65b9d4ed47b6287fa279cb66706883
whoiam <whoiam1101@outlook.com>
2022-05-03 21:32:14 +0400

    I don't know.

--- data structure/trees/node.hpp
+++ data structure/trees/node.hpp
@@ -8,6 +8,20 @@ struct Node {
 
     Node() : _left(nullptr), _right(nullptr) {}
     Node(int data) : _left(nullptr), _right(nullptr), _data(data) {}
+
+    void _clearAll();
 };
+
+void Node::_clearAll() {
+    Node* left = _left;
+    Node* right = _right;
+    delete this;
+    if (left) {
+        left->_clearAll();
+    }
+    if (right) {
+        right->_clearAll();
+    }
+}
 
 #endif // __NODE_H__
