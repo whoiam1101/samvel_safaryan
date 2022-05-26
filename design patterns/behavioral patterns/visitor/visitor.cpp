dc0d5d96266beeba8c0c36e020dda4affafe7207
whoiam <whoiam1101@outlook.com>
2022-05-26 19:59:26 +0400

    visitor - behavioral pattern

---
+++ design patterns/behavioral patterns/visitor/visitor.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+using namespace std;
+
+enum EnumCompanies {
+  FACEBOOK = 0,
+  TELEGRAM,
+  TWITTER
+};
+
+class Facebook;
+class Telegram;
+class Twitter;
+
+class Visitor {
+public:
+  virtual ~Visitor() {}
+
+  virtual void visit(Facebook&) = 0;
+  virtual void visit(Telegram&) = 0;
+  virtual void visit(Twitter&)  = 0;
+};
+
+class Company {
+public:
+  virtual ~Company() {}
+
+  virtual void accept(Visitor&) = 0;
+};
+
+class Facebook : public Company {
+public:
+  void accept(Visitor& visitor) override { visitor.visit(*this); }
+};
+
+class Telegram : public Company {
+public:
+  void accept(Visitor& telegram) override { telegram.visit(*this); }
+};
+
+class Twitter : public Company {
+public:
+  void accept(Visitor& twitter) override { twitter.visit(*this); }
+};
+
+class HumanVisitor : public Visitor {
+private:
+  EnumCompanies _messenger;
+
+public:
+  void visit(Facebook& facebook) override { _messenger = EnumCompanies::FACEBOOK; }
+  void visit(Telegram& telegram) override { _messenger = EnumCompanies::TELEGRAM; }
+  void visit(Twitter&  twitter)  override { _messenger = EnumCompanies::TWITTER; }
+
+  void whatCompany() const {
+    switch (_messenger) {
+    case EnumCompanies::FACEBOOK: cout << "I'm in facebook now!\n"; break;
+    case EnumCompanies::TELEGRAM: cout << "I'm in telegram now!\n"; break;
+    case EnumCompanies::TWITTER:  cout << "I'm in twitter  now!\n";   break;
+    }
+  }
+};
+
+int main() {
+  Company* facebook = new Facebook();
+  Company* telegram = new Telegram();
+  Company* twitter  = new Twitter();
+  HumanVisitor human_visitor;
+
+  facebook->accept(human_visitor);
+  human_visitor.whatCompany();
+
+  telegram->accept(human_visitor);
+  human_visitor.whatCompany();
+
+  twitter->accept(human_visitor);
+  human_visitor.whatCompany();
+
+  return 0;
+}
